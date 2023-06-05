3ca481ff918de44105cdc016c9d3ca4c118a3a39
Mike <mike.x@binance.com>
2023-06-05 14:39:04 +0900

    Update os version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "KeychainSwift",
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "KeychainSwift", targets: ["KeychainSwift"]),
     ],
